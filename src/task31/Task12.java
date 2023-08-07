7db5e2d79df7307e9573241a5fcff8ac49360d4e
ZeynepA <zeyneppaytop@gmail.com>
2023-08-07 22:51:29 +0300

    tasks 31 added

---
+++ src/task31/Task12.java
@@ -0,0 +1,12 @@
+package task31;
+
+public class Task12 {
+
+	public static void main(String[] args) {
+		//Task ->  Asagida verilen map'de yazilim dili java olan kisi isimlerini bir liste return eden method create edinizn
+
+
+	}
+	
+
+}
